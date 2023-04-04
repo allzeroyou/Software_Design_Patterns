81b7fb9551928b91c67c1c5315b464a121256ffd
allzeroyou <ekdudd119@naver.com>
2023-04-04 09:36:18 +0900

    다중 스레드 환경일 경우 Singleton 패턴 적용 안되는 문제 해결

--- DP_2023-1/src/ch05/A3a/Singleton.java
+++ DP_2023-1/src/ch05/A3a/Singleton.java
@@ -8,7 +8,8 @@ public class Singleton {
         slowdown(); // 생성될 때 시간이 걸리도록 함. // 여러 스레드 왔다갔다 할 수 있도록 해 다른 객체를 생성하도록
     }
 
-    public static Singleton getInstance() { // 싱글톤 객체 생성.
+    // public static Singleton getInstance() { // 싱글톤 객체 생성.
+    public static synchronized Singleton getInstance() { // synchronized: 동시에 2개 이상의 스레드가 getInstance() 안으로 들오지 못하도록 막는다.
         if (singleton == null) {
             singleton = new Singleton();
         }
