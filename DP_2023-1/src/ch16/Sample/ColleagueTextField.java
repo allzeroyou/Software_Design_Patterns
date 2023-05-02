cb8f942bcdad6223cf865e16d4565dc3b24b78b1
allzeroyou <ekdudd119@naver.com>
2023-05-02 10:17:05 +0900

    Mediator 패턴: 중재자를 두어 로직을 집중시킨다

--- DP_2023-1/src/ch16/Sample/ColleagueTextField.java
+++ DP_2023-1/src/ch16/Sample/ColleagueTextField.java
@@ -5,30 +5,31 @@ import java.awt.TextField;
 import java.awt.event.TextEvent;
 import java.awt.event.TextListener;
 
-public class ColleagueTextField extends TextField implements TextListener, Colleague {
-    private Mediator mediator;
+public class ColleagueTextField extends TextField implements TextListener, Colleague { // Colleague 역할.
+  // TextListener: 텍스트입력시 이벤트 리스너
+  private Mediator mediator;
 
-    public ColleagueTextField(String text, int columns) {
-        super(text, columns);
-    }
+  public ColleagueTextField(String text, int columns) { // text: 기본
+    super(text, columns); // TextField의 생성자
+  }
 
-    // Mediator를 설정한다 
-    @Override
-    public void setMediator(Mediator mediator) {
-        this.mediator = mediator;
-    }
+  // Mediator를 설정한다
+  @Override
+  public void setMediator(Mediator mediator) {
+    this.mediator = mediator;
+  }
 
-    // Mediator에서 활성/비활성을 지시한다
-    @Override
-    public void setColleagueEnabled(boolean enabled) {
-        setEnabled(enabled);
-        // 활성/비활성에 맞게 배경색을 변경한다
-        setBackground(enabled ? Color.white : Color.lightGray);
-    }
+  // Mediator에서 활성/비활성을 지시한다
+  @Override
+  public void setColleagueEnabled(boolean enabled) {
+    setEnabled(enabled);
+    // 활성/비활성에 맞게 배경색을 변경한다
+    setBackground(enabled ? Color.white : Color.lightGray);
+  }
 
-    @Override
-    public void textValueChanged(TextEvent e) {
-        // 문자열이 변화했으면 Mediator에 알린다
-        mediator.colleagueChanged();
-    }
+  @Override
+  public void textValueChanged(TextEvent e) {
+    // 문자열이 변화했으면 Mediator에 알린다
+    mediator.colleagueChanged();
+  }
 }
--- DP_2023-1/src/ch16/Sample/Main.java
+++ DP_2023-1/src/ch16/Sample/Main.java
@@ -2,6 +2,7 @@ package ch16.Sample;
 
 public class Main {
     static public void main(String[] args) {
+       // 프레임 생성
         new LoginFrame("Mediator Sample");
     }
 }
