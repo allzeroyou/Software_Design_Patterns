07620420e2183615d4690b4b89ebbc3e3576a4e1
allzeroyou <ekdudd119@naver.com>
2023-05-11 13:33:47 +0900

    Flyweight 패턴: 메모리를 가볍게, 인스턴스를 가능한 공유시킨다

--- DP_2023-1/src/ch20/Sample/BigChar.java
+++ DP_2023-1/src/ch20/Sample/BigChar.java
@@ -5,29 +5,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class BigChar {
-    // 문자의 이름 
-    private char charname;
-    // 큰 문자를 표현하는 문자열('#' '.' '\n'으로 이루어진 열)
-    private String fontdata;
+  // 문자의 이름
+  private char charname;
+  // 큰 문자를 표현하는 문자열('#' '.' '\n'으로 이루어진 열)
+  private String fontdata; // 파일의 내용을 담고 있음.
 
-    // 생성자 
-    public BigChar(char charname) {
-        this.charname = charname;
-        try {
-            String filename = "big" + charname + ".txt";
-            StringBuilder sb = new StringBuilder();
-            for (String line: Files.readAllLines(Path.of(filename))) {
-                sb.append(line);
-                sb.append("\n");
-            }
-            this.fontdata = sb.toString();
-        } catch (IOException e) {
-            this.fontdata = charname + "?";
-        }
+  // 생성자
+  public BigChar(char charname) { // '1'
+    this.charname = charname;
+    try {
+      String filename = "big" + charname + ".txt";
+      StringBuilder sb = new StringBuilder();
+      for (String line : Files.readAllLines(Path.of(filename))) { // file 위치 지정.
+        sb.append(line);
+        sb.append("\n"); 
+      }
+      this.fontdata = sb.toString();
+    } catch (IOException e) {
+      this.fontdata = charname + "?";
     }
+  }
 
-    // 큰 문자를 표시한다
-    public void print() {
-        System.out.print(fontdata);
-    }
+  // 큰 문자를 표시한다
+  public void print() {
+    System.out.print(fontdata);
+  }
 }
